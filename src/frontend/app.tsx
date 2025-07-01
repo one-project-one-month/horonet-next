1cddb5c22b3beebed63342eb03f492b94c4933b7
mk <myatkw629@gmail.com>
2025-07-01 21:17:17 +0630

    fixed(for real this time)

--- src/frontend/app.tsx
+++ src/frontend/app.tsx
@@ -39,6 +39,7 @@ const router = createBrowserRouter([
           },
           {
             path: "compatibility",
+            loader: authLoader,
             element: <CompatibilityPage />,
           },
         ],
