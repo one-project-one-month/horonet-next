a955743b2f72a0da25669d97673953869fe0d4ad
mk <myatkw629@gmail.com>
2025-07-06 17:51:24 +0630

    switched to pagination

--- src/components/compatibility/compatible-people.tsx
+++ src/components/compatibility/compatible-people.tsx
@@ -30,94 +30,45 @@ const CompatiblePeople = ({ peopleList, compatibleSignsInfo }: { peopleList: Com
     })[0].score;
   };
 
-  const filterCompatiblePeople = (sign: string) => {
-    setCompatiblePeople(peopleList.filter((p) => {
-      return p.sign === sign;
-    }));
-  };
-
-  const resetCompatiblePeopleFilter = () => {
-    setCompatiblePeople(peopleList);
-  };
-
   return (
-    <div>
-      {compatiblePeople
-        ? (
-            <div className="container w-2/4 mx-auto">
-              <div className="flex justify-center gap-3">
-                {
-                  compatibleSignsInfo
-                  && (
-                    <Button onClick={resetCompatiblePeopleFilter} className="text-yellow-300">
-                      <h4 className="object-contain w-full h-full">All</h4>
-                    </Button>
-                  )
-                }
-                {
-                  compatibleSignsInfo && compatibleSignsInfo.map((s) => {
-                    return (
-                      <Button key={s.compatibleSignId} onClick={() => { filterCompatiblePeople(s.compatibleSignName); }}>
-                        <Image src={`${BASE_SVG_PATH}/${s.compatibleSignName}.svg`} alt={`${s.compatibleSignName}.svg`} className="object-contain w-full h-full" width={50} height={50} />
-                      </Button>
-                    );
-                  })
-                }
+    <div className="flex justify-center mt-4">
+      <ScrollArea className="border-background/50 px-1.5 min-w-[25rem] lg:min-w-[50rem] md:min-w-[50rem] sm:min-w-[50rem] rounded-md border">
+        {
+          compatiblePeople && compatiblePeople.length > 0 && compatiblePeople.map((p) => {
+            const SignIcon = getIconFromSign(p.sign as TZodiacSigns);
+            return (
+              <div key={p.id} className="flex flex-col bg-cosmic-purple backdrop-blur-sm border-background/20 rounded-md border mt-2.5 mb-2.5 text-xs sm:text-[16px] md:text-[16px] lg:text-[16px]">
+                <div className="flex justify-between p-2">
+                  <div className="flex flex-col">
+                    <h1 className="flex font-bold mb-1 text-sm sm:text-[16px] md:text-[16px] lg:text-[16px]">
+                      {p.name}
+                      <Image src={`${BASE_SVG_PATH}/${p.gender}.svg`} alt="gender.svg" width={25} height={25} className="object-contain" />
+                    </h1>
+                    <SignIcon className="size-10 fill-cosmic-deep-purple p-1 h-full rounded-full bg-gradient-to-r from-cosmic-gold via-cosmic-starlight to-cosmic-gold" />
+                  </div>
+                  <h3>
+                    Score: {getScore(p.sign)}
+                  </h3>
+                </div>
+                <div className="flex flex-col text-center mb-6">
+                  <h3>Chemistry: {getChemistry(p.sign)}</h3>
+                </div>
+                <div className="flex justify-between px-2 py-2">
+                  <Button className="bg-rose-400 border-1 border-rose-500 hover:bg-rose-500">
+                    <Flower />
+                  </Button>
+                  <Button className="bg-gradient-to-r from-cosmic-deep-purple via-cosmic-gold to-cosmic-starlight">
+                    <span>View Profile</span>
+                  </Button>
+                  <Button className="bg-amber-400 border-1 border-amber-500 hover:bg-amber-500">
+                    <CookieIcon />
+                  </Button>
+                </div>
               </div>
-
-              <div className="flex justify-center mt-4">
-                <ScrollArea className="border-background/50 px-1.5 min-w-[25rem] lg:min-w-[50rem] md:min-w-[50rem] sm:min-w-[50rem] rounded-md border">
-                  {
-                    compatiblePeople.length > 0
-
-                      ? compatiblePeople.map((p) => {
-                          const SignIcon = getIconFromSign(p.sign as TZodiacSigns);
-                          return (
-                            <div key={p.id} className="flex flex-col bg-cosmic-purple backdrop-blur-sm border-background/20 rounded-md border mt-2.5 mb-2.5 text-xs sm:text-[16px] md:text-[16px] lg:text-[16px]">
-                              <div className="flex justify-between p-2">
-                                <div className="flex flex-col">
-                                  <h1 className="flex font-bold mb-1 text-sm sm:text-[16px] md:text-[16px] lg:text-[16px]">
-                                    {p.name}
-                                    <Image src={`${BASE_SVG_PATH}/${p.gender}.svg`} alt="gender.svg" width={25} height={25} className="object-contain" />
-                                  </h1>
-                                  <SignIcon className="size-10 fill-cosmic-deep-purple p-1 h-full rounded-full bg-gradient-to-r from-cosmic-gold via-cosmic-starlight to-cosmic-gold" />
-                                </div>
-                                <h3>
-                                  Score: {getScore(p.sign)}
-                                </h3>
-                              </div>
-                              <div className="flex flex-col text-center mb-6">
-                                <h3>Chemistry: {getChemistry(p.sign)}</h3>
-                              </div>
-                              <div className="flex justify-between px-2 py-2">
-                                <Button className="bg-rose-400 border-1 border-rose-500 hover:bg-rose-500">
-                                  <Flower />
-                                </Button>
-                                <Button className="bg-gradient-to-r from-cosmic-deep-purple via-cosmic-gold to-cosmic-starlight">
-                                  <span>View Profile</span>
-                                </Button>
-                                <Button className="bg-amber-400 border-1 border-amber-500 hover:bg-amber-500">
-                                  <CookieIcon />
-                                </Button>
-                              </div>
-                            </div>
-                          );
-                        })
-                      : (
-                          <div>
-                            Found none.
-                          </div>
-                        )
-                  }
-                </ScrollArea>
-              </div>
-            </div>
-          )
-        : (
-            <div>
-              There's nothing here yet.
-            </div>
-          )}
+            );
+          })
+        }
+      </ScrollArea>
     </div>
   );
 };
--- src/frontend/pages/compatibility.tsx
+++ src/frontend/pages/compatibility.tsx
@@ -1,5 +1,3 @@
-import type { ChangeEvent } from "react";
-
 import { useState } from "react";
 
 import type { CompatiblePeopleInfo, CompatibleSignsInfo } from "@/components/compatibility/compatibility-custom-types";
@@ -7,22 +5,20 @@ import type { CompatiblePeopleInfo, CompatibleSignsInfo } from "@/components/com
 import CompatiblePeople from "@/components/compatibility/compatible-people";
 import WiseWords from "@/components/compatibility/wise-words";
 import { Button } from "@/components/ui/button";
-import { Checkbox } from "@/components/ui/checkbox";
-import { Input } from "@/components/ui/input";
 import { trpc } from "@/trpc/clitent";
 
-export const CompatibilityPage = () => {
-  const trpcUtil = trpc.useUtils();
-  const [limitSearch, setLimitSearch] = useState<boolean>(false);
-  const [userLimit, setUserLimit] = useState<number>(10);
-  const [userLimitParam, setUserLimitParam] = useState<number | null>(null);
+;
 
+export const CompatibilityPage = () => {
+  const [page, setPage] = useState<number>(0);
   const getCurrentUserSign = trpc.compatibility.getCurrentUserSign.useQuery();
   const findCompatibleSigns = trpc.compatibility.findCompatibleSigns.useQuery(getCurrentUserSign.data?.signId as string);
   const findCompatiblePeople = trpc.compatibility.findCompatiblePeople.useQuery({
     compatibleSignIds: findCompatibleSigns.data?.map((d) => { return d.compatibleSignId; }) as string[],
-    limit: limitSearch ? userLimitParam : null,
-  }, { enabled: !!userLimitParam });
+    page,
+    limit: 3,
+  }, { enabled: true });
+
   // Dynamic {enabled} based on userLimitParam to stop it from fetching on initial render.
   // Passing in 'userLimit' directly causes it to refetch on every state change when setting limits via input.
   // Used a second state called 'userLimitParam' to trigger fetch.
@@ -31,66 +27,19 @@ export const CompatibilityPage = () => {
     return <div>Loading...</div>;
   }
 
-  const flipLimitSearch = () => {
-    setLimitSearch(!limitSearch);
-  };
-
-  const decreaseUserLimit = () => {
-    if (userLimit > 0) {
-      setUserLimit(userLimit - 1);
+  const prevPage = () => {
+    if (page > 0) {
+      setPage(page - 1);
     }
   };
 
-  const increaseUserLimit = () => {
-    setUserLimit(userLimit + 1);
-  };
-
-  const manualUserLimitChange = (e: ChangeEvent<HTMLInputElement>) => {
-    let newValue;
-
-    if (e.target.value === "") {
-      newValue = 0;
-    }
-    else {
-      newValue = Number(e.target.value);
-    }
-
-    if (newValue >= 0) {
-      setUserLimit(newValue);
-    }
-  };
-
-  const findCompatiblePeopleFunc = () => {
-    trpcUtil.compatibility.findCompatiblePeople.invalidate(); // Clear cache
-    // Changing this state causes trpc to fetch.
-    // This also set a value in 'userLimitParam', which enables the {enabled} field.
-    setUserLimitParam(userLimit);
+  const nextPage = () => {
+    setPage(page + 1);
   };
 
   return (
     <div className="text-center">
       <WiseWords sign={getCurrentUserSign.data?.signName as string} />
-      {/* Refactor later */}
-      <Button
-        className="mt-4"
-        variant={"cosmic"}
-        onClick={() => {
-          findCompatiblePeopleFunc();
-        }}
-      >Find companions
-      </Button>
-
-      <div className="flex justify-center mt-4 gap-1.5">
-        <Button disabled={!limitSearch} onClick={increaseUserLimit}>+</Button>
-        <Input disabled={!limitSearch} value={userLimit} type="text" onChange={manualUserLimitChange} className="object-contain w-24 text-center" />
-        <Button disabled={!limitSearch} onClick={decreaseUserLimit}>-</Button>
-      </div>
-
-      <div className="flex justify-center mt-2 gap-1.5 items-center mb-8">
-        <Checkbox id="limitCheckBox" checked={limitSearch} onClick={flipLimitSearch} />
-        <label htmlFor="limitCheckBox">Limit search</label>
-      </div>
-
       {
         findCompatiblePeople.isLoading || findCompatiblePeople.isFetching
           ? (
@@ -98,7 +47,15 @@ export const CompatibilityPage = () => {
                 Finding...
               </div>
             )
-          : <CompatiblePeople peopleList={findCompatiblePeople.data as CompatiblePeopleInfo[]} compatibleSignsInfo={findCompatibleSigns.data as CompatibleSignsInfo[]} />
+          : (
+              <div className="container mx-auto">
+                <CompatiblePeople peopleList={findCompatiblePeople.data?.compatiblePeople as CompatiblePeopleInfo[]} compatibleSignsInfo={findCompatibleSigns.data as CompatibleSignsInfo[]} />
+                <div className="flex justify-center gap-16 mt-6">
+                  <Button variant={"cosmic"} disabled={!findCompatiblePeople.data?.hasPrev} onClick={prevPage}>Prev</Button>
+                  <Button variant={"cosmic"} disabled={!findCompatiblePeople.data?.hasNext} onClick={nextPage}>Next</Button>
+                </div>
+              </div>
+            )
       }
     </div>
   );
