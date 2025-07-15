f6552dd66fb50e10658704eb35cb15d1d1838bf7
humbl3LilaS <sabishinekobebe@gmail.com>
2025-07-15 22:28:40 +0630

    add: Loading State initial JS loading

--- src/app/shell/page.tsx
+++ src/app/shell/page.tsx
@@ -2,12 +2,27 @@
 
 import dynamic from "next/dynamic";
 
-const App = dynamic(() => import("@/frontend/app"), { ssr: false });
+import LoadingSpinner from "@/components/common/loading-spinner";
+
+const App = dynamic(() => import("@/frontend/app"), {
+  ssr: false,
+  loading: () => (
+    <section className={"w-screen h-screen flex justify-center items-center "}>
+      <div>
+        <LoadingSpinner />
+        <h2 className={"mt-16 text-2xl text-white font-bold text-center"}>
+          Connecting to the Cosmos...
+        </h2>
+        <p className={"text-white/40 text-center"}>
+          The stars are aligning your experience
+        </p>
+      </div>
+    </section>
+  ),
+});
 
 const ShellPage = () => {
-  return (
-    <App />
-  );
+  return <App />;
 };
 
 export default ShellPage;
